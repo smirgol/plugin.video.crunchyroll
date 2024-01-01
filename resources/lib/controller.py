ade63eb375ba84a9c1d8eb1fb59af73ef9203777
smirgol <14124899+smirgol@users.noreply.github.com>
2024-01-01 21:25:55 +0100

    comment out thumbs and fanart which rely on args

--- resources/lib/controller.py
+++ resources/lib/controller.py
@@ -570,8 +570,8 @@ def view_series(args, api: API):
                     "aired": None,  # item["created"][:10],
                     "premiered": None,  # item["created"][:10],
                     "status": u"Completed" if item["is_complete"] else u"Continuing",
-                    "thumb": args.thumb,
-                    "fanart": args.fanart,
+                    # "thumb": args.thumb,  # @todo: re-add
+                    # "fanart": args.fanart, # @todo: re-add
                     "mode": "episodes"
                 },
                 is_folder=True
@@ -649,9 +649,9 @@ def view_episodes(args, api: API):
                     "plotoutline": item["description"],
                     "aired": item["episode_air_date"][:10],
                     "premiered": item["availability_starts"][:10],  # ???
-                    "poster": args.thumb,
+                    # "poster": args.thumb,  # @todo: re-add
                     "thumb": utils.get_image_from_struct(item, "thumbnail", 2),
-                    "fanart": args.fanart,
+                    # "fanart": args.fanart,  # @todo: re-add
                     "mode": "videoplay",
                     # note that for fetching streams we need a special guid, not the episode_id
                     "stream_id": stream_id,
