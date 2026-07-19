f9484b84465722037a255dcff34381ec363ce0a7
smirgol <14124899+smirgol@users.noreply.github.com>
2026-07-19 13:13:42 +0200

    Add polish language, update auth
    - update auth
    - add polish language
    - update readme
    - bump version to 3.6.11

--- resources/lib/auth.py
+++ resources/lib/auth.py
@@ -39,7 +39,7 @@ from .utils.datetime import date_to_str, get_date, str_to_date
 from .utils.logging import crunchy_log
 
 # Authentication credentials - single device-only identity (AndroidTV for device auth)
-AUTHORIZATION = "Basic bm1oaGcwbDZ4eXhjZm02aHQ2aGY6SjR6bU1mdjNkMVFkWHk4dDk2d1NjeDdoUnkzclBHLTM="
+AUTHORIZATION = "Basic bWMyNDBjZTc1dzduYmRoYmIwcTY6RWN5c2JiV2RYbzFSMFdXQ1RFNTVEUGxabndXNC1PNWI="
 
 TOKEN_ENDPOINT = "https://www.crunchyroll.com/auth/v1/token"
 DEVICE_CODE_ENDPOINT = "https://www.crunchyroll.com/auth/v1/device/code"
